e109b42e23fd2e8002f4c1bd5a6ff9cbdb33f160
8309h <harshalwagh201718@gmail.com>
2024-05-09 18:19:22 +0530

    frontend done till category

---
+++ Frontend/script/dashboard.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const logoutButton = document.getElementById("logoutbutton");
+
+    const confirmdiv = document.getElementById('logout_confimdiv')
+    confirmdiv.style.display = 'none'
+
+    logoutButton.addEventListener("click", function(e) {
+        e.preventDefault()
+        console.log("button hits...")
+        confirmdiv.style.display = 'block'
+        confirmdiv.style.position= "fixed"
+        confirmdiv.style.top = "100px";
+        confirmdiv.style.left = "90%";
+        confirmdiv.style.height = "200px"
+        confirmdiv.style.backgroundColor ='whitesmoke'
+        confirmdiv.style.color ='black'
+
+
+        const confirmlogoutbutton = document.getElementById('confirm_logout')
+        confirmlogoutbutton.addEventListener('click',function(){
+            fetch("http://localhost:8080/user/logout", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                
+            }
+        })
+        .then(response => {
+            if (response.message ="Logout successful" ) {
+                alert("Logout successful");
+                window.location.href = "index.html";
+            } else {
+                alert("Logout failed");
+            }
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            alert("Logout failed. Please try again.");
+        });
+
+        })
+
+
+        
+    });
+
+
+    const cancel_logout = document.getElementById('cancel')
+    cancel_logout.addEventListener('click', function(){
+        confirmdiv.style.display = 'none'
+    })
+});
---
+++ Frontend/script/index.js
@@ -0,0 +1,140 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const loginForm = document.getElementById("loginForm");
+    const registerForm = document.getElementById("registerForm");
+    const showRegisterBtn = document.getElementById("showRegisterForm");
+    const showLoginFormBtn = document.getElementById("showLoginForm");
+
+    registerForm.style.display = "none";
+
+    showRegisterBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+        loginForm.style.display = "none";
+        registerForm.style.display = "block";
+    });
+
+    showLoginFormBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+        registerForm.style.display = "none";
+        loginForm.style.display = "block";
+    });
+
+})
+
+    
+
+
+let form = document.getElementById('registerForm');
+form.addEventListener('submit', myfun);
+
+function myfun(event) {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (email === "" || password === "") {
+        alert("All fields are required");
+    } else {
+        let payload = {email, password,};
+        let deployed = "http://localhost:8080/user/register";
+
+        fetch(deployed, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(payload)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log("data", data.msg);
+
+            if (data.msg === "User already exists. Please login") {
+                alert(data.msg);
+                location.href = "index.html";
+            } else if (data.msg === "New user registered successfully") {
+                alert("SignUp Success");
+                location.href = "login.html";
+                
+                loginForm.style.display = "block";
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }
+}
+
+
+// Function for login form
+function handleLoginForm(event) {
+    event.preventDefault();
+
+    const loginEmail = document.getElementById("loginEmail").value;
+    const loginPassword = document.getElementById("loginPassword").value;
+
+    if (loginEmail === "" || loginPassword === "") {
+        alert("All fields are required");
+        return false;
+    }
+
+    const payload = { email: loginEmail, password: loginPassword };
+    console.log("payload", payload);
+
+    const deployedUrl = "http://localhost:8080/user/login";
+
+    fetch(deployedUrl, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify(payload),
+    })
+    .then((res) => {
+        console.log("res",res)
+        if (!res.ok) {
+            alert("Invalid Credentials")
+        }
+        return res.json();
+    })
+    .then((res) => {
+        console.log("userback", res.user);
+
+        if (res.msg === "Login successful") {
+            localStorage.setItem("digitoken", res.token);
+            localStorage.setItem("haslogin", true);
+            const userObject = res.user;
+            const userJSON = JSON.stringify(userObject);
+            localStorage.setItem("login_user", userJSON);
+            alert("Login Successful");
+            location.href = "dashboard.html";
+        } else {
+            alert("Login failed");
+        }
+    })
+    .catch((err) => {
+        console.error("Error:", err);
+    });
+}
+
+const loginForm = document.getElementById("loginForm");
+loginForm.addEventListener("submit", handleLoginForm);
+
+
+function togglePasswordVisibility() {
+    const passwordField = document.getElementById("password");
+    const eyeIcon = document.getElementById("eye");
+
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        eyeIcon.textContent = "👁️";
+    } else {
+        passwordField.type = "password";
+        eyeIcon.textContent = "🔒";
+    }
+}
+
+
+
+
+
