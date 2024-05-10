80834a4b03c48930aeecb5e7bb4c767681652851
8309h <harshalwagh201718@gmail.com>
2024-05-10 16:54:25 +0530

    searh category backend done

--- Backend/routes/categoryRoutes.js
+++ Backend/routes/categoryRoutes.js
@@ -56,4 +56,47 @@ CategoryRouter.post("/addnew", async (req, res) => {
 });
 
 
+
+CategoryRouter.patch('/update/:id', async (req, res) => {
+  const categoryId = req.params.id;
+  const { id, name, description, status } = req.body; 
+
+  try {
+    const updatedCategory = await CategoryModel.findByIdAndUpdate(categoryId, { id, name, description, status }, { new: true });
+
+    if (!updatedCategory) {
+      return res.status(404).json({ error: 'Category not found' });
+    }
+
+    res.status(200).json(updatedCategory);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// route for search by name
+
+
+CategoryRouter.get('/search', async (req, res) => {
+  const { name } = req.query;
+
+  try {
+    const categories = await CategoryModel.find({ name: { $regex: new RegExp(name, 'i') } });
+
+    if (categories.length === 0) {
+      return res.status(404).json({ error: 'No categories found matching the search criteria' });
+    }
+
+    res.status(200).json(categories);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+
+
+
 module.exports = CategoryRouter;
